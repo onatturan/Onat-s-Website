785d9ce5078b7feb856a7d94690ea846ff75bef9
OnatTuran <onatturann@gmail.com>
2025-01-26 23:59:39 -0500

    Değişiklik açıklaması

--- server.js
+++ server.js
@@ -1,27 +1,64 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const SERVER_PORT = 3000;
+const socketio = require('socket.io');
 
-// HTML dosyasını sunma
-app.use(express.static('public'));  // HTML dosyasının olduğu klasörü belirtin
-
-// Socket.io ile iletişim kurma
-io.on("connection", (socket) => {
-    console.log("a user connected");
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
-    });
-    socket.on("disconnect", () => {
-        console.log("user disconnected");
-    });
+// Sunucu başlatma
+const server = app.listen(SERVER_PORT, () => {
+    console.log(`Chat Server running on http://localhost:3000/`);
 });
 
-// Sunucuyu başlatma
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+// Statik dosyaları sunma
+app.use(express.static('views'));
+
+// Sayfaların yönlendirilmesi
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/chat.html');
+});
+
+app.get("/group", (req, res) => {
+    res.sendFile(__dirname + '/views/group_chat.html');
+});
+
+// Socket.io kurulum
+const io = socketio(server);
+
+// Yeni bağlantılar
+io.on('connection', (socket) => {
+    console.log(`New Socket: ${socket.id}`);
+    
+    // Kullanıcı bağlantısı kesildiğinde
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+    
+    // Mesaj alındığında
+    socket.on('message', (data) => {
+        console.log(`Message from ${socket.id}: ${data}`);
+    });
+
+    // Sohbet mesajı alındığında
+    socket.on('chat_message', (data) => {
+        data.clientId = socket.id;
+        console.log(JSON.stringify(data));
+        io.emit('chat_message', data);
+    });
+
+    // Grup sohbetine katılma
+    socket.on('join_group', (roomName) => {
+        console.log(`User ${socket.id} joined room ${roomName}`);
+        socket.join(roomName);
+    });
+
+    // Grup sohbetinden çıkma
+    socket.on('leave_group', (roomName) => {
+        socket.leave(roomName);
+    });
+
+    // Grup mesajı gönderme
+    socket.on('group_message', (data) => {
+        console.log(`User ${socket.id} sent message to room ${data.group}`);
+        data.senderId = socket.id;
+        io.to(data.group).emit('group_message', data);
+    });
 });
